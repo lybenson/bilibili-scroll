26019ae21db896cde50d979899bec8853f985937
lybenson <naivebenson@gmail.com>
2017-03-29 23:45:22 +0800

    ui

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <div class="container-row" v-for="(item, index) in items" :id="'b_' + item.desc">
+        <h1>{{ item.name }}</h1>
+      </span>
+    </div>
+    <navc :items="items"></navc>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import navc from './components/bilibili-scroll'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    navc
+  },
+  data() {
+    return {
+      items: [{
+        name: '动画',
+        desc: 'douga',
+      }, {
+        name: '游戏',
+        desc: 'game',
+      }, {
+        name: '音乐',
+        desc: 'music',
+      }, {
+        name: '舞蹈',
+        desc: 'dance',
+      }, {
+        name: '科技',
+        desc: 'technology',
+      }, {
+        name: '生活',
+        desc: 'life',
+      }, {
+        name: '电影',
+        desc: 'movie',
+      }]
+    }
+  },
+  computed: {
+    options() {
+      let options = {
+        offset: 100,
+        data: this.data,
+        backToTop: 0
+      }
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: "Segoe UI", 'Microsoft Yahei', Helvetica, Arial;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.container-row {
+  width: 100%;
+  height: 200px;
+  font: 30px;
+  margin-bottom: 20px;
+  background-color: green;
+}
+h1 {
+  height: 200px;
+  line-height: 200px;
 }
 </style>
---
+++ src/components/bilibili-scroll.vue
@@ -0,0 +1,95 @@
+<template>
+	<div class="navt">
+		<transition>
+			<div>
+				<div class="tip"></div>
+				<div class="custom-bg"></div>
+			</div>
+		</transition>
+		<div class="nav-list">
+			<div class="n-i sotrable" :class="{on: current===index}" v-for="(item, index) in items">
+				<div class="name">{{item.name}}</div>
+			</div>
+			<div class="n-i customize">
+				<i class="n-icon-sort"></i>
+				<p>排序</p>
+			</div>
+		</div>
+		<div class="n-i gotop">
+			<div class="s-line"></div>
+			<div class="btn_gotop"></div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			current: 0
+		}
+	},
+	props: {
+		items: {
+			type: Array
+		}
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+	.navt
+		position fixed
+		width 48px
+		text-align center
+		top 0px
+		left auto
+		right 0px
+		.nav-list
+			position relative
+			background-color #f6f9fa
+			border 1px solid #e5e9ef
+			overflow hidden
+			border-radius 4px
+			.n-i
+				cursor pointer
+				&:first-child
+					border-top 0
+				&.on
+					.name
+						background-color #00a1d6
+						color #fff
+				&.customize
+					padding 8px 0
+					border-top 1px solid #e5e9ef
+					.n-icon-sort
+						display block
+						margin 0 auto 4px
+						background url(../assets/icons.png) -663px -151px no-repeat
+						height 18px
+						width 18px
+				.name
+					height 32px
+					line-height 32px
+					transition .1s linear
+					transition-property background-color, color
+					&:hover
+						background-color #00a1d6
+						color #fff
+		.gotop
+			cursor pointer
+			border 0
+			position relative
+			.s-line
+				border-left 1px solid #ddd
+				border-right 1px solid #ddd
+				height 9px
+				width 30px
+				margin 0 auto
+			.btn_gotop
+				height 48px
+				background #f6f9fa url(../assets/icons.png) -648px -72px no-repeat
+				border 1px solid #e5e9ef
+				overflow hidden
+				border-radius 4px
+</style>
